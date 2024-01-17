612f42b2dc8b7d0b9d454b7a258b5c00ea670016
gabrielCarvalhoGit <gabriel.carvalhogss@gmail.com>
2024-01-17 00:46:22 -0300

    CRUD Documento

--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Documento
 
-# Create your views here.
+class DocumentoCreate(CreateView):
+    model = Documento
+    fields = ['descricao', 'arquivo']
